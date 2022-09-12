7f7b62f3dde012fda1cb20846bb67ae85627974c
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-12 08:54:08 +0700

    Add types to App.Locals: wp_fetch, wp_info

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,8 +1,13 @@
+/* eslint-disable space-in-parens */
+
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		wp_fetch: import('$types').wp_fetch;
+		wp_info: import('$types').WP_Info;
+	}
 	// interface PageData {}
 	// interface PageError {}
 	// interface Platform {}
