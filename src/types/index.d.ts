d94cd6e5d18e69a66a8909923f9c90e3c5f6b76c
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-12 08:52:59 +0700

    Add types: wp_fetch, WP_Info

---
+++ src/types/index.d.ts
@@ -0,0 +1,13 @@
+export type wp_fetch = ( path: string, options?: RequestInit ) => Promise< Response >;
+
+export interface WP_Info {
+	authentication: string[];
+	description: string;
+	gmt_offset: number;
+	home: string;
+	name: string;
+	site_icon: number;
+	site_logo: number;
+	timezone_string: string;
+	url: string;
+}
