a1cb0904ce5832036123fe35e2fafe979cdba7a5
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-13 06:52:00 +0700

    Add helper for wp_fetch: generate_rest_url()

--- src/lib/api/make-wp-fetch.js
+++ src/lib/api/make-wp-fetch.js
@@ -1,11 +1,29 @@
 /** @typedef {import('@sveltejs/kit').Handle} Handle */
 
 /**
+ * Generate REST URL.
+ *
+ * @param {string} root REST root URL.
+ * @param {string} path REST API path.
+ * @return {string} Generated REST URL.
+ */
+function generate_rest_url( root, path ) {
+	if ( ! path.startsWith( '/' ) ) {
+		return path;
+	}
+
+	let url = root.replace( /[/]+$/, '' );
+	url += url.includes( '?' ) ? path.replace( '?', '&' ) : path;
+
+	return url;
+}
+
+/**
  * Make wp_fetch function
  *
  * @param {Object}   config          Configuration object.
  * @param {boolean=} config.add_info Whether the site info object should always be added to event.locals as `wp_info`.
- * @param {string}   config.root     WordPress REST API URL.
+ * @param {string}   config.root     WordPress REST API root URL.
  *
  * @return {Handle} Handle function.
  */
@@ -14,7 +32,7 @@ export function make_wp_fetch( config ) {
 
 	/** @type {import('$types').wp_fetch} */
 	async function wp_fetch( path, options = undefined ) {
-		const resource = path.startsWith( '/' ) ? root + path : path;
+		const resource = generate_rest_url( root, path );
 
 		// TODO: Add nonce if provided in the config.
 		// TODO: Refresh nonce when needed if the URL is provided in the config.
