a305bb8609fc3cb4c9207f81f1d3e8f4a3e5a6e0
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-12 12:08:00 +0700

    Rename file

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,5 +1,5 @@
 import { sequence } from '@sveltejs/kit/hooks';
-import { make_wp_fetch } from './lib/api/hooks';
+import { make_wp_fetch } from './lib/api/make-wp-fetch';
 import { PUBLIC_WP_API_ROOT_URL } from '$env/static/public';
 
 export const handle = sequence( make_wp_fetch( { add_info: true, root: PUBLIC_WP_API_ROOT_URL } ) );
