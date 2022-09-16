a5583d2c033a6f649eac3982f557a9a788afd26b
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-17 06:50:07 +0700

    Frontpage server load: Split frontpage fetcher

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -7,21 +7,16 @@ function frontpage_error() {
 	// TODO: Log.
 }
 
-/** @type {import('./$types').PageServerLoad} */
-export async function load( { locals } ) {
-	if ( ! WP_FRONTPAGE_ID ) {
-		frontpage_error();
-	}
-
-	const frontpage_id = Number( WP_FRONTPAGE_ID );
-
-	if ( isNaN( frontpage_id ) || frontpage_id < 1 ) {
-		frontpage_error();
-	}
-
-	const { wp_fetch } = locals;
+/**
+ * Fetch frontpage
+ *
+ * @param {import('$types').wp_fetch} wp_fetch WP fetch.
+ * @param {number}                    id       Frontpage ID.
+ * @return {Promise<import('wp-types').WP_REST_API_Post>} Post object.
+ */
+async function fetch_frontpage( wp_fetch, id ) {
 	try {
-		const response = await wp_fetch( `/wp/v2/pages/${ frontpage_id }` );
+		const response = await wp_fetch( `/wp/v2/pages/${ id }` );
 
 		if ( ! response.ok ) {
 			throw await response.json();
@@ -30,7 +25,7 @@ export async function load( { locals } ) {
 		/** @type {import('wp-types').WP_REST_API_Post} */
 		const post = await response.json();
 
-		return { post };
+		return post;
 	} catch ( err ) {
 		/** @type {import('wp-types').WP_REST_API_Error} */
 		let wp_api_error;
@@ -44,4 +39,22 @@ export async function load( { locals } ) {
 		// TODO.
 		throw err;
 	}
+}
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load( { locals } ) {
+	if ( ! WP_FRONTPAGE_ID ) {
+		frontpage_error();
+	}
+
+	const frontpage_id = Number( WP_FRONTPAGE_ID );
+
+	if ( isNaN( frontpage_id ) || frontpage_id < 1 ) {
+		frontpage_error();
+	}
+
+	const { wp_fetch } = locals;
+	const post = await fetch_frontpage( wp_fetch, frontpage_id );
+
+	return { post };
 }
