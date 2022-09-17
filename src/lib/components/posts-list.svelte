fea1cf7da5d836e1cb5925e4faaa1ba4527cb67b
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-17 08:29:02 +0700

    Add component: PostsList

---
+++ src/lib/components/posts-list.svelte
@@ -0,0 +1,17 @@
+<script>
+	import Time from './time.svelte';
+
+	/** @type {import('wp-types').WP_REST_API_Post[]} */
+	export let posts;
+	export let title = '';
+	export let title_tag = 'h2';
+</script>
+
+{#if title}
+	<svelte:element this={title_tag}>{title}</svelte:element>
+{/if}
+<ul>
+	{#each posts as post}
+		<li><Time datetime={post.date} /> <span>{@html post.title.rendered}</span></li>
+	{/each}
+</ul>
