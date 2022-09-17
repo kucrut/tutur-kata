cae2d848ae2d5c33058fa574f15889e69c9ce965
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-17 08:55:07 +0700

    Add single post page component

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<article>
+	<h1>{data.post.title.rendered}</h1>
+	<div>{@html data.post.content.rendered}</div>
+</article>
