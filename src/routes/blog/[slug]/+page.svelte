43dcccfbf465eb2df61bea98c4c1290e4943d1ee
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-17 09:19:01 +0700

    Single blog page: Render doc title

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -3,6 +3,10 @@
 	export let data;
 </script>
 
+<svelte:head>
+	<title>{data.title}</title>
+</svelte:head>
+
 <article>
 	<h1>{data.post.title.rendered}</h1>
 	<div>{@html data.post.content.rendered}</div>
