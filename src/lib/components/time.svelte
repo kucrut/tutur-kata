c511bd251dde0db818dd20a88a41287f99837b7a
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-17 08:28:42 +0700

    Add component: Time

---
+++ src/lib/components/time.svelte
@@ -0,0 +1,17 @@
+<script>
+	/** @type {string} */
+	export let datetime;
+	// TODO: Get timezone from wp_info.timezone_string.
+	/** @type {Intl.DateTimeFormatOptions['timeZone']} */
+	export let time_zone = undefined;
+
+	const date = new Date( Date.parse( datetime ) );
+	const display = new Intl.DateTimeFormat( undefined, {
+		day: 'numeric',
+		month: '2-digit',
+		year: 'numeric',
+		timeZone: time_zone,
+	} ).format( date );
+</script>
+
+<time {datetime}>{display}</time>
