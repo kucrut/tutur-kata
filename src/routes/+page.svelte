1a9abfc52359fb1771287fd24691b6efcc30ff15
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-18 16:09:05 +0700

    Front page: Use Article component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script>
-	import simple_entity_decode from '$lib/utils/simple-entity-decode';
+	import Article from '$lib/components/article.svelte';
 	import PostsList from '$lib/components/posts-list.svelte';
 	import Seo from 'svelte-seo';
 
@@ -10,10 +10,7 @@
 <Seo title={data.title} />
 
 <div class="container">
-	<article>
-		<h1>{simple_entity_decode( data.wp_info.name )}</h1>
-		<div>{@html data.post.content.rendered}</div>
-	</article>
+	<Article post={data.post} show_title={false} />
 
 	{#if data.latest_posts?.length}
 		<PostsList posts={data.latest_posts} title="Latest Posts" />
