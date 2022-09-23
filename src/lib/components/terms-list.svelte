6069dd691d6e990394d83d57bf5e4a05152eb0d0
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-23 23:03:22 +0700

    Add component: TermsList

---
+++ src/lib/components/terms-list.svelte
@@ -0,0 +1,28 @@
+<script>
+	import simple_entity_decode from '$lib/utils/simple-entity-decode';
+
+	let cls = '';
+
+	export { cls as class };
+	/** @type {import('wp-types').WP_REST_API_Taxonomy} */
+	export let taxonomy;
+	/** @type {import('wp-types').WP_REST_API_Term[]} */
+	export let terms;
+
+	// TODO: Accept item_component prop.
+</script>
+
+<slot name="before-list" {taxonomy} {terms} />
+
+<ul
+	aria-label={simple_entity_decode( taxonomy.name )}
+	class="terms-list {cls}"
+	data-hierarchical={taxonomy.hierarchical}
+	data-taxonomy={taxonomy.slug}
+>
+	{#each terms as term}
+		<li>{simple_entity_decode( term.name )}</li>
+	{/each}
+</ul>
+
+<slot name="after-list" {taxonomy} {terms} />
