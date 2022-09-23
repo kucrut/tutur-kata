291e2454081c9d006bc2bd0a481534277b555ab4
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-23 23:03:49 +0700

    Article: Render TermsList in after-content slot

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Article from '$lib/components/article.svelte';
 	import Seo from 'svelte-seo';
+	import TermsList from '$lib/components/terms-list.svelte';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
@@ -9,5 +10,13 @@
 <Seo title={data.title} />
 
 <div class="container">
-	<Article post={data.post} terms={data.terms} />
+	<Article post={data.post} terms={data.terms}>
+		<svelte:fragment slot="after-content" let:terms>
+			{#if terms?.length}
+				{#each terms as post_terms}
+					<TermsList taxonomy={post_terms.taxonomy} terms={post_terms.terms} />
+				{/each}
+			{/if}
+		</svelte:fragment>
+	</Article>
 </div>
