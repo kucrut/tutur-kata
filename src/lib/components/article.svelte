a049ad2d1cc1bc887e9943012047f8899b520532
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-23 22:38:46 +0700

    Article: Accept terms prop and pass it to slots

--- src/lib/components/article.svelte
+++ src/lib/components/article.svelte
@@ -9,12 +9,14 @@
 	/** @type {import('wp-types').WP_REST_API_Post} */
 	export let post;
 	export let show_title = true;
+	/** @type {import('$lib/api/utils.server').Post_Terms[]|null} */
+	export let terms;
 	export let title_class = '';
 	export let title_tag = 'h1';
 </script>
 
 <article class={cls || null}>
-	<slot name="before-title" {link_title} {post} {show_title} {title_tag} />
+	<slot name="before-title" {link_title} {post} {show_title} {terms} {title_tag} />
 
 	{#if show_title}
 		<svelte:element this={title_tag} class={title_class || null}>
@@ -26,9 +28,9 @@
 		</svelte:element>
 	{/if}
 
-	<slot name="before-content" {link_title} {post} {show_title} {title_tag} />
+	<slot name="before-content" {link_title} {post} {show_title} {terms} {title_tag} />
 
 	<div class={content_class || null}>{@html post.content.rendered}</div>
 
-	<slot name="after-content" {link_title} {post} {show_title} {title_tag} />
+	<slot name="after-content" {link_title} {post} {show_title} {terms} {title_tag} />
 </article>
