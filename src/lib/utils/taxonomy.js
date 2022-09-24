1364bc579052fd2e7b47fa19707feeddf065cdc2
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-24 12:12:36 +0700

    Add taxonomy data processor

---
+++ src/lib/utils/taxonomy.js
@@ -0,0 +1,20 @@
+/**
+ * @typedef {import('wp-types').WP_REST_API_Taxonomy} Taxonomy
+ */
+
+import { decode_entities } from '$lib/utils/simple-entity-decode';
+
+/**
+ * Process WP taxonomy data
+ *
+ * @param {Taxonomy} taxonomy WP taxonomy object.
+ *
+ * @return {Taxonomy} Processed WP taxonomy object.
+ */
+export function process_taxonomy( taxonomy ) {
+	return {
+		...taxonomy,
+		description: decode_entities( taxonomy.description ),
+		name: decode_entities( taxonomy.name ),
+	};
+}
