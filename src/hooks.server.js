e4fc8bf61d989ada51e758d2fde2487fbae0cce4
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-24 20:35:37 +0700

    Rename file

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,5 +1,5 @@
 import { sequence } from '@sveltejs/kit/hooks';
-import { make_wp_fetch } from './lib/api/make-wp-fetch.server';
+import { make_wp_fetch } from './lib/api/wp-fetch.server';
 import { WP_API_APP_AUTH, WP_API_ROOT_URL } from '$env/static/private';
 
 export const handle = sequence( make_wp_fetch( { add_info: true, app_auth: WP_API_APP_AUTH, root: WP_API_ROOT_URL } ) );
