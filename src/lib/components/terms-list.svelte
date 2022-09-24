68f8c5fe9ad526bb2c38f363b941b8fbc1a15a5d
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-24 12:14:58 +0700

    TermsList: No need to decode taxonomy & term name

--- src/lib/components/terms-list.svelte
+++ src/lib/components/terms-list.svelte
@@ -1,6 +1,4 @@
 <script>
-	import { decode_entities } from '$lib/utils/simple-entity-decode';
-
 	let cls = '';
 
 	export { cls as class };
@@ -16,14 +14,9 @@
 
 <slot name="before-list" {taxonomy} {terms} />
 
-<ul
-	aria-label={decode_entities( taxonomy.name )}
-	class={classes}
-	data-hierarchical={taxonomy.hierarchical}
-	data-taxonomy={taxonomy.slug}
->
+<ul aria-label={taxonomy.name} class={classes} data-hierarchical={taxonomy.hierarchical} data-taxonomy={taxonomy.slug}>
 	{#each terms as term}
-		<li>{decode_entities( term.name )}</li>
+		<li>{term.name}</li>
 	{/each}
 </ul>
 
