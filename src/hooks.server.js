10b6587fc1a6b3ac9f22b491f558b8b464a75de8
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-25 16:02:09 +0700

    Clean-up

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,13 +1,15 @@
-/** @typedef {import('@sveltejs/kit').Handle} Handle */
-
 import { fetch_info } from '$lib/api/utils.server';
 import { sequence } from '@sveltejs/kit/hooks';
 
-/** @type {Handle} */
-async function add_info( { event, resolve } ) {
+/**
+ * Add wp_info to locals
+ *
+ * @type {import('@sveltejs/kit').Handle}
+ */
+async function add_wp_info_to_locals( { event, resolve } ) {
 	event.locals.wp_info = await fetch_info();
 
 	return await resolve( event );
 }
 
-export const handle = sequence( add_info );
+export const handle = sequence( add_wp_info_to_locals );
