09704073162a91a5b721dd6ffbe22cbe0eb9d398
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-26 19:36:51 +0700

    TermsList: Remove default class name

--- src/lib/components/terms-list.svelte
+++ src/lib/components/terms-list.svelte
@@ -6,13 +6,16 @@
 	export let taxonomy;
 	/** @type {import('wp-types').WP_REST_API_Term[]} */
 	export let terms;
-
-	$: classes = `terms-list ${ cls }`.trim();
 </script>
 
 <slot name="before-list" {taxonomy} {terms} />
 
-<ul aria-label={taxonomy.name} class={classes} data-hierarchical={taxonomy.hierarchical} data-taxonomy={taxonomy.slug}>
+<ul
+	aria-label={taxonomy.name}
+	class={cls || null}
+	data-hierarchical={taxonomy.hierarchical}
+	data-taxonomy={taxonomy.slug}
+>
 	{#each terms as term}
 		<li>
 			{#if $$slots.item}
