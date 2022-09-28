a297c14007d82557f934a7b1ec836d0574379e57
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-28 21:20:30 +0700

    Add route: /blog

---
+++ src/routes/blog/+page.server.js
@@ -0,0 +1,15 @@
+import { fetch_latest_posts } from '$lib/api/utils.server';
+import { generate_doc_title } from '$lib/utils/seo';
+
+/** @type {import('./$types').PageServerLoad} */
+export async function load( { locals } ) {
+	const posts = await fetch_latest_posts();
+
+	return {
+		posts,
+		title: generate_doc_title( locals.wp_info, {
+			title: 'Blog',
+			type: 'archive',
+		} ),
+	};
+}
---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import PostsList from '$lib/components/posts-list.svelte';
+	import Seo from 'svelte-seo';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+</script>
+
+<Seo title={data.title} />
+
+<div class="container">
+	{#if data.posts?.length}
+		<PostsList posts={data.posts} title="Latest Posts" />
+	{/if}
+</div>
