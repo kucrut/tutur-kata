3ec6e3876e6ed6d6ae546bb73d952f3d72d46299
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-28 21:27:05 +0700

    Blog archive: Add fallback content

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -11,5 +11,8 @@
 <div class="container">
 	{#if data.posts?.length}
 		<PostsList posts={data.posts} title="Latest Posts" />
+	{:else}
+		<h1>Nothing found</h1>
+		<p>There are no blog posts to show at this time. Stay tuned! 😉</p>
 	{/if}
 </div>
