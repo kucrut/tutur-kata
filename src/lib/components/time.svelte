697bca8e0d150e19988bafd06ba415737a77ea01
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-30 22:31:46 +0700

    Time: Get wp_info from $page.data

--- src/lib/components/time.svelte
+++ src/lib/components/time.svelte
@@ -1,18 +1,15 @@
 <script>
-	import { getContext } from 'svelte';
+	import { page } from '$app/stores';
 
 	/** @type {string} */
 	export let datetime;
-
-	/** @type {import('$types').WP_Info} */
-	const wp_info = getContext( 'wp_info' );
 
 	const date = new Date( Date.parse( datetime ) );
 	const display = new Intl.DateTimeFormat( undefined, {
 		day: 'numeric',
 		month: '2-digit',
 		year: 'numeric',
-		timeZone: wp_info.timezone_string,
+		timeZone: $page.data.wp_info.timezone_string,
 	} ).format( date );
 </script>
 
