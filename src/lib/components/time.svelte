b050c0900893e7f8c0788b3cadf4d4c055f43dbc
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-30 22:42:22 +0700

    Time: Use get_datetime_locale()

--- src/lib/components/time.svelte
+++ src/lib/components/time.svelte
@@ -1,11 +1,12 @@
 <script>
+	import { get_datetime_locale } from '$lib/utils/locale';
 	import { page } from '$app/stores';
 
 	/** @type {string} */
 	export let datetime;
 
 	const date = new Date( Date.parse( datetime ) );
-	const display = new Intl.DateTimeFormat( undefined, {
+	const display = new Intl.DateTimeFormat( get_datetime_locale(), {
 		day: 'numeric',
 		month: '2-digit',
 		year: 'numeric',
