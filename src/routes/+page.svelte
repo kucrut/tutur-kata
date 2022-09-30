ae18b7ae7714f9068282efad2985357f31677ea9
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-30 16:38:42 +0700

    Frontpage: Render intro with good ol div

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script>
-	import Article from '$lib/components/article.svelte';
 	import PostsList from '$lib/components/posts-list.svelte';
 	import Seo from 'svelte-seo';
 
@@ -10,7 +9,7 @@
 <Seo title={data.title} />
 
 <div class="container">
-	<Article post={data.post} show_title={false} />
+	<div class="frontpage-intro">{@html data.post.content.rendered}</div>
 
 	{#if data.latest_posts?.length}
 		<PostsList posts={data.latest_posts} title="Latest Posts" />
