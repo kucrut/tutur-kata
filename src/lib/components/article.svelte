8e6926e20fdce51ce9369b3572000e6b7255f56a
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-30 16:45:48 +0700

    Article: Remove props: link_title, show_title

--- src/lib/components/article.svelte
+++ src/lib/components/article.svelte
@@ -3,16 +3,14 @@
 
 	export { cls as class };
 	export let content_class = '';
-	export let link_title = false;
 	/** @type {import('wp-types').WP_REST_API_Post} */
 	export let post;
-	export let show_title = true;
 	/** @type {import('$lib/api/utils.server').Post_Terms[]|null} */
 	export let terms = null;
 </script>
 
 <article class={cls || null}>
-	<slot name="before-title" {link_title} {post} {show_title} {terms} />
+	<slot name="before-title" {post} {terms} />
 
 	{#if $$slots.title}
 		<slot name="title" {post} {terms} />
@@ -20,9 +18,9 @@
 		<h1>{post.title.rendered}</h1>
 	{/if}
 
-	<slot name="before-content" {link_title} {post} {show_title} {terms} />
+	<slot name="before-content" {post} {terms} />
 
 	<div class={content_class || null}>{@html post.content.rendered}</div>
 
-	<slot name="after-content" {link_title} {post} {show_title} {terms} />
+	<slot name="after-content" {post} {terms} />
 </article>
