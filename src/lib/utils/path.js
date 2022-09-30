20e46ea96afd114c73681039993daad849eebc40
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-30 17:16:43 +0700

    Add path helper: get_blog_path()

--- src/lib/utils/path.js
+++ src/lib/utils/path.js
@@ -1,11 +1,20 @@
 /** @typedef {import('wp-types').WP_REST_API_Post} Post */
 
 /**
+ * Get blog path
+ *
+ * @return {string} Blog path.
+ */
+export function get_blog_path() {
+	return '/blog';
+}
+
+/**
  * Get post link
  *
  * @param {Post} post Post object.
  * @return {string} Post link.
  */
 export function get_link( post ) {
-	return `/blog/${ post.slug }`;
+	return `${ get_blog_path() }/${ post.slug }`;
 }
