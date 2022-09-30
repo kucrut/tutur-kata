c0729aaa0514d686058480605b58e6be9de7997e
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-30 17:39:48 +0700

    Time: Use timezone from wp_info context

--- src/lib/components/time.svelte
+++ src/lib/components/time.svelte
@@ -1,16 +1,18 @@
 <script>
+	import { getContext } from 'svelte';
+
 	/** @type {string} */
 	export let datetime;
-	// TODO: Get timezone from wp_info.timezone_string.
-	/** @type {string|undefined} */
-	export let time_zone = undefined;
+
+	/** @type {import('$types').WP_Info} */
+	const wp_info = getContext( 'wp_info' );
 
 	const date = new Date( Date.parse( datetime ) );
 	const display = new Intl.DateTimeFormat( undefined, {
 		day: 'numeric',
 		month: '2-digit',
 		year: 'numeric',
-		timeZone: time_zone,
+		timeZone: wp_info.timezone_string,
 	} ).format( date );
 </script>
 
