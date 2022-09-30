810f7f36946bcc5f723e8d99653be43f80e8a702
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-30 16:41:19 +0700

    PostsList: Accept class

--- src/lib/components/posts-list.svelte
+++ src/lib/components/posts-list.svelte
@@ -1,11 +1,13 @@
 <script>
 	import Time from './time.svelte';
 
+	let cls = '';
+	export { cls as class };
 	/** @type {import('wp-types').WP_REST_API_Posts} */
 	export let posts;
 </script>
 
-<ul>
+<ul class={cls || null}>
 	{#each posts as post}
 		<li><Time datetime={post.date} /> <a href="/blog/{post.slug}">{post.title.rendered}</a></li>
 	{/each}
