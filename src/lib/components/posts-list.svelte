4808d672a25d89eba41a892cd521c39f99b74c37
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-30 16:40:24 +0700

    PostsList: Don't render title

--- src/lib/components/posts-list.svelte
+++ src/lib/components/posts-list.svelte
@@ -1,15 +1,10 @@
 <script>
 	import Time from './time.svelte';
 
-	/** @type {import('wp-types').WP_REST_API_Post[]} */
+	/** @type {import('wp-types').WP_REST_API_Posts} */
 	export let posts;
-	export let title = '';
-	export let title_tag = 'h2';
 </script>
 
-{#if title}
-	<svelte:element this={title_tag}>{title}</svelte:element>
-{/if}
 <ul>
 	{#each posts as post}
 		<li><Time datetime={post.date} /> <a href="/blog/{post.slug}">{post.title.rendered}</a></li>
