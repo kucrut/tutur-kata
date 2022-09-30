c26d8531e774741e704d3ee24776c438c1f58fd2
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-09-30 16:54:03 +0700

    PostsList: Accept get_link() for generating post link

--- src/lib/components/posts-list.svelte
+++ src/lib/components/posts-list.svelte
@@ -1,14 +1,25 @@
 <script>
+	/** @typedef {import('wp-types').WP_REST_API_Post} Post */
+
 	import Time from './time.svelte';
 
 	let cls = '';
 	export { cls as class };
-	/** @type {import('wp-types').WP_REST_API_Posts} */
+
+	/** @type {Post[]} */
 	export let posts;
+
+	/**
+	 * Get post link
+	 *
+	 * @param {Post} post Post object.
+	 * @return {string} Post link.
+	 */
+	export let get_link = post => `/${ post.slug }`;
 </script>
 
 <ul class={cls || null}>
 	{#each posts as post}
-		<li><Time datetime={post.date} /> <a href="/blog/{post.slug}">{post.title.rendered}</a></li>
+		<li><Time datetime={post.date} /> <a href={get_link( post )}>{post.title.rendered}</a></li>
 	{/each}
 </ul>
