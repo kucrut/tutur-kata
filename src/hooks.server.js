c645fd97a571f3d7f6248cb2375e0360275c1115
Dzikri Aziz <dzikri.aziz@gmail.com>
2022-11-27 14:58:57 +0700

    Handle error in server hooks

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,3 +1,4 @@
+import { error } from '@sveltejs/kit';
 import { fetch_info } from '$lib/api/utils.server';
 import { sequence } from '@sveltejs/kit/hooks';
 
@@ -7,9 +8,12 @@ import { sequence } from '@sveltejs/kit/hooks';
  * @type {import('@sveltejs/kit').Handle}
  */
 async function add_wp_info_to_locals( { event, resolve } ) {
-	event.locals.wp_info = await fetch_info();
-
-	return await resolve( event );
+	try {
+		event.locals.wp_info = await fetch_info();
+		return await resolve( event );
+	} catch ( err ) {
+		throw error( 500, err instanceof Error ? err : { message: 'Unknown error.' } );
+	}
 }
 
 export const handle = sequence( add_wp_info_to_locals );
