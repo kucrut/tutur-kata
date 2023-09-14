1a6a2ea66eba533feab6b44b2b68f6ea4a87d1a7
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-15 01:39:56 +0700

    postcss config: Replace nested with nesting

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
 	plugins: [
 		require( 'postcss-import' ),
 		require( 'postcss-custom-media' ),
-		require( 'postcss-nested' ),
+		require( 'postcss-nesting' ),
 		require( 'autoprefixer' ),
 	],
 };
