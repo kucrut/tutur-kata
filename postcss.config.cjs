071aa34dc48c6dd42624803d680f724d5fbf346a
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-15 15:07:16 +0700

    Update postcss config
    Add open props, preset env.

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const jit_props = require( 'postcss-jit-props' );
+const open_props = require( 'open-props' );
+const preset_env = require( 'postcss-preset-env' );
+
 module.exports = {
 	plugins: [
 		require( 'postcss-import' ),
 		require( 'postcss-custom-media' ),
 		require( 'postcss-nesting' ),
 		require( 'autoprefixer' ),
+		jit_props( open_props ),
+		preset_env( { stage: 0 } ),
 	],
 };
