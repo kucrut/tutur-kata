0f7242d7bda8f63a3e9a5ab0dffd934a3872ef15
Dzikri Aziz <dzikri.aziz@gmail.com>
2023-09-16 00:49:14 +0700

    Update postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,16 +1,22 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
+const autoprefixer = require( 'autoprefixer' );
+const at_import = require( 'postcss-import' );
+const global_data = require( '@csstools/postcss-global-data' );
 const jit_props = require( 'postcss-jit-props' );
 const open_props = require( 'open-props' );
 const preset_env = require( 'postcss-preset-env' );
 
 module.exports = {
 	plugins: [
-		require( 'postcss-import' ),
-		require( 'postcss-custom-media' ),
-		require( 'postcss-nesting' ),
-		require( 'autoprefixer' ),
+		at_import,
+		global_data( {
+			files: [
+				'node_modules/open-props/media.min.css',
+			],
+		} ),
 		jit_props( open_props ),
 		preset_env( { stage: 0 } ),
+		autoprefixer,
 	],
 };
