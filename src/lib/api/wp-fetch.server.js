bf69f9e31e676cea3ff50b0e85b2b0d5f942a016
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-04 18:39:47 +0700

    Add helper: get_api_auth()

--- src/lib/api/wp-fetch.server.js
+++ src/lib/api/wp-fetch.server.js
@@ -20,6 +20,25 @@ export function generate_rest_url( root, path ) {
 }
 
 /**
+ * Get WP API auth
+ *
+ * @return {string} WP API auth string.
+ */
+export function get_api_auth() {
+	const auth = env.WP_API_APP_AUTH || '';
+
+	if ( ! auth ) {
+		return auth;
+	}
+
+	if ( env.WP_API_APP_AUTH_TYPE === 'jwt' ) {
+		return `Bearer ${ auth }`;
+	}
+
+	return `Basic ${ Buffer.from( auth ).toString( 'base64' ) }`;
+}
+
+/**
  * Get WP API root URL
  *
  * @return {string} WP Rest API root URL.
