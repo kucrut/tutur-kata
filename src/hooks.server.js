14e1264b1d4e97297d709ed4f8f1e3f9afc5384c
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-04 18:53:38 +0700

    Hooks (server): Decode entities in site name & description

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,4 +1,5 @@
 import { get_api_auth, get_api_url } from '$lib/api/wp-fetch.server';
+import { decode_entities } from '$lib/utils/simple-entity-decode';
 import { get_info } from '@kucrut/wp-api-helpers';
 import { sequence } from '@sveltejs/kit/hooks';
 
@@ -8,7 +9,13 @@ import { sequence } from '@sveltejs/kit/hooks';
  * @type {import('@sveltejs/kit').Handle}
  */
 async function add_wp_info_to_locals( { event, resolve } ) {
-	event.locals.wp_info = await get_info( get_api_url(), get_api_auth() );
+	const wp_info = await get_info( get_api_url(), get_api_auth() );
+
+	event.locals.wp_info = {
+		...wp_info,
+		description: decode_entities( wp_info.description ),
+		name: decode_entities( wp_info.name ),
+	};
 
 	return await resolve( event );
 }
