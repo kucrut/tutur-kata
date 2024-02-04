ec44d741e4ac413d263fd16b99caea5bec3f761f
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-05 00:24:20 +0700

    Remove obsolete functions

--- src/lib/api/wp-fetch.server.js
+++ src/lib/api/wp-fetch.server.js
@@ -1,23 +1,4 @@
 import { env } from '$env/dynamic/private';
-import { error } from '@sveltejs/kit';
-
-/**
- * Generate REST URL
- *
- * @param {string} root REST root URL.
- * @param {string} path REST API path.
- * @return {string} Generated REST URL.
- */
-export function generate_rest_url( root, path ) {
-	if ( ! path.startsWith( '/' ) ) {
-		return path;
-	}
-
-	let url = root.replace( /[/]+$/, '' );
-	url += url.includes( '?' ) ? path.replace( '?', '&' ) : path;
-
-	return url;
-}
 
 /**
  * Get WP API auth
@@ -45,38 +26,4 @@ export function get_api_auth() {
  */
 export function get_api_url() {
 	return env.WP_API_ROOT_URL;
-}
-
-/**
- * Fetch data from WordPress
- *
- * @param {string}                 path    REST API path.
- * @param {RequestInit|undefined=} options Fetch options.
- *
- * @return {Promise<Response>} Response object.
- */
-export async function wp_fetch( path, options = undefined ) {
-	const resource = generate_rest_url( env.WP_API_ROOT_URL, path );
-	const request = new Request( resource, options );
-
-	// TODO: Add cookie & nonce if passed (highest priority).
-	// TODO: Refresh nonce when needed if the URL is provided in the config.
-
-	if ( env.WP_API_APP_AUTH ) {
-		request.headers.append(
-			'Authorization',
-			`Basic ${ Buffer.from( `${ env.WP_API_APP_AUTH }` ).toString( 'base64' ) }`,
-		);
-	}
-
-	const response = await fetch( request );
-
-	if ( response.ok ) {
-		return response;
-	}
-
-	// eslint-disable-next-line no-console
-	console.error( { path, status: response.status, message: response.statusText } );
-
-	error( 500, 'Internal error' );
 }
