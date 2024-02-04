184c8885888b3e2356ad31626bad1a7ae289a77f
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-04 22:40:22 +0700

    generate_favicons(): Use get_media() from wp-api-helpers

--- src/lib/api/utils.server.js
+++ src/lib/api/utils.server.js
@@ -1,7 +1,6 @@
 /**
  * @typedef {import('$types').Favicon} Favicon
  * @typedef {import('$types').TileImage} TileImage
- * @typedef {import('$types').WP_REST_API_Media} WP_REST_API_Media
  * @typedef {import('wp-types').WP_REST_API_Post} Post
  * @typedef {import('wp-types').WP_REST_API_Term} Term
  * @typedef {import('wp-types').WP_REST_API_Taxonomy} Taxonomy
@@ -9,11 +8,12 @@
  * @typedef {{taxonomy: Taxonomy, terms: Term[]}} Post_Terms
  */
 
+import { get_media } from '@kucrut/wp-api-helpers';
 import { process_post_data } from '$lib/utils/post';
 import { maybe_throw_wp_api_error } from './utils';
 import { process_taxonomy_data } from '$lib/utils/taxonomy';
 import { process_term_data } from '$lib/utils/term';
-import { wp_fetch } from './wp-fetch.server';
+import { get_api_auth, get_api_url, wp_fetch } from './wp-fetch.server';
 
 /**
  * Generate favicons
@@ -26,40 +26,44 @@ export async function generate_favicons( site_icon_id ) {
 		return null;
 	}
 
-	const response = await wp_fetch( `/wp/v2/media/${ site_icon_id }` );
+	try {
+		const attachment = await get_media( get_api_url(), get_api_auth(), site_icon_id );
 
-	if ( ! response.ok ) {
-		return null;
-	}
+		if ( ! attachment?.media_details?.sizes ) {
+			return null;
+		}
 
-	const key_prefix = 'site_icon-';
-	/** @type {WP_REST_API_Media} */
-	const attachment = await response.json();
-	/** @type {Icons} */
-	const icons = [];
+		/** @type {Icons} */
+		const icons = [];
+		const key_prefix = 'site_icon-';
 
-	for ( const size of [ 32, 180, 192 ] ) {
-		const key = key_prefix + size;
+		for ( const size of [ 32, 180, 192 ] ) {
+			const key = key_prefix + size;
 
-		if ( key in attachment.media_details.sizes ) {
+			if ( key in attachment.media_details.sizes ) {
+				icons.push( {
+					href: attachment.media_details.sizes[ key ].source_url,
+					rel: size === 180 ? 'apple-touch-icon' : 'icon',
+					sizes: `${ size }x${ size }`,
+				} );
+			}
+		}
+
+		const tile_key = key_prefix + '270';
+
+		if ( tile_key in attachment.media_details.sizes ) {
 			icons.push( {
-				href: attachment.media_details.sizes[ key ].source_url,
-				rel: size === 180 ? 'apple-touch-icon' : 'icon',
-				sizes: `${ size }x${ size }`,
+				content: attachment.media_details.sizes[ tile_key ].source_url,
+				name: 'msapplication-TileImage',
 			} );
 		}
+
+		return icons;
+	} catch ( error ) {
+		// eslint-disable-next-line no-console
+		console.error( error );
+		return null;
 	}
-
-	const tile_key = key_prefix + '270';
-
-	if ( tile_key in attachment.media_details.sizes ) {
-		icons.push( {
-			content: attachment.media_details.sizes[ tile_key ].source_url,
-			name: 'msapplication-TileImage',
-		} );
-	}
-
-	return icons;
 }
 
 /**
