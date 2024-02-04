8041ed248b7c216a2e09cc5b1ff268cef0359324
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-04 20:23:12 +0700

    Homepage: Use get_posts() from wp-api-helpers to get frontpage data

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,43 +1,35 @@
-/** @typedef {import('wp-types').WP_REST_API_Post} Post */
-
 import { env } from '$env/dynamic/private';
 import { error } from '@sveltejs/kit';
 import { fetch_latest_posts } from '$lib/api/utils.server';
 import { generate_doc_title } from '$lib/utils/seo';
+import { get_api_auth, get_api_url } from '$lib/api/wp-fetch.server';
+import { get_posts } from '@kucrut/wp-api-helpers';
 import { maybe_throw_wp_api_error } from '$lib/api/utils';
 import { process_post_data } from '$lib/utils/post';
-import { wp_fetch } from '$lib/api/wp-fetch.server';
 
 /**
- * Frontpage error
+ * Get frontpage
  *
- * @param {string} message Error message.
+ * @return {Promise<import('$types').WP_Post>} Post object.
  */
-function frontpage_error( message ) {
-	// eslint-disable-next-line no-console
-	console.error( message );
-	error( 500 );
-}
+async function get_frontpage() {
+	if ( ! env.WP_FRONTPAGE_ID ) {
+		// eslint-disable-next-line no-console
+		console.error( 'WP_FRONTPAGE_ID is not set.' );
+		error( 500 );
+	}
 
-/**
- * Fetch frontpage
- *
- * @param {number} id Frontpage ID.
- *
- * @return {Promise<Post>} Post object.
- */
-async function fetch_frontpage( id ) {
+	const frontpage_id = Number( env.WP_FRONTPAGE_ID );
+
+	if ( isNaN( frontpage_id ) || frontpage_id < 1 ) {
+		// eslint-disable-next-line no-console
+		console.error( 'Invalid WP_FRONTPAGE_ID value.' );
+		error( 500 );
+	}
+
 	try {
-		const response = await wp_fetch( `/wp/v2/pages/${ id }` );
-
-		if ( ! response.ok ) {
-			throw await response.json();
-		}
-
-		/** @type {Post} */
-		const post_raw = await response.json();
-
-		return await process_post_data( post_raw );
+		const post = await get_posts( get_api_url(), get_api_auth(), 'pages', frontpage_id );
+		return await process_post_data( post );
 	} catch ( err ) {
 		maybe_throw_wp_api_error( err );
 		throw err; // TODO.
@@ -46,22 +38,9 @@ async function fetch_frontpage( id ) {
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load( { locals } ) {
-	if ( ! env.WP_FRONTPAGE_ID ) {
-		frontpage_error( 'WP_FRONTPAGE_ID is not set.' );
-	}
-
-	const frontpage_id = Number( env.WP_FRONTPAGE_ID );
-
-	if ( isNaN( frontpage_id ) || frontpage_id < 1 ) {
-		frontpage_error( 'Invalid WP_FRONTPAGE_ID value.' );
-	}
-
-	const post = await fetch_frontpage( frontpage_id );
-	const latest_posts = await fetch_latest_posts();
-
 	return {
-		latest_posts,
-		post: await process_post_data( post ),
+		latest_posts: await fetch_latest_posts(),
+		post: await get_frontpage(),
 		title: generate_doc_title( locals.wp_info, { type: 'frontpage' } ),
 	};
 }
