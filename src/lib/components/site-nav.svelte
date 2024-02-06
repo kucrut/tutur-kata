76ff874539fa142c911d8eb249970866ae85448b
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-06 11:42:54 +0700

    SiteNav: Add aria-current attribute to items

--- src/lib/components/site-nav.svelte
+++ src/lib/components/site-nav.svelte
@@ -1,13 +1,17 @@
 <script>
 	import { get_blog_path } from '$lib/utils/path';
 	import { page } from '$app/stores';
+
+	const blog_path = get_blog_path();
+
+	$: aria_current = ( /** @type {string} */ path ) => ( $page.url.pathname === path ? 'page' : null );
 </script>
 
 <nav class="container site-nav">
 	<span class="site-branding">
-		<a href="/">{$page.data.wp_info.name}</a>
+		<a aria-current={aria_current( $page.url.pathname )} href="/">{$page.data.wp_info.name}</a>
 	</span>
 
-	<a href={get_blog_path()}>Blog</a>
-	<a href="/gallery">Gallery</a>
+	<a aria-current={aria_current( blog_path )} href={blog_path}>Blog</a>
+	<a aria-current={aria_current( '/gallery' )} href="/gallery">Gallery</a>
 </nav>
