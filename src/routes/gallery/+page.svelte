d21538f37d4653d9cb4484a9a9930bc0a0ddd0a1
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-08 01:19:43 +0700

    Gallery page: Show full-size image modal on click

--- src/routes/gallery/+page.svelte
+++ src/routes/gallery/+page.svelte
@@ -1,10 +1,31 @@
 <script>
+	import { page } from '$app/stores';
+	import { pushState } from '$app/navigation';
 	import Image from '$lib/components/image.svelte';
 	import Masonry from '$lib/components/masonry.svelte';
+	import Popover from '$lib/components/popover.svelte';
 	import Seo from 'svelte-seo';
 
 	/** @type {import('./$types').PageData} */
 	export let data;
+	/** @type {import('$types').WP_Media|undefined} */
+	let current_media;
+
+	/**
+	 * Show modal
+	 *
+	 * @param {import('$types').WP_Media} item Media item.
+	 * @param {MouseEvent} event Mouse event.
+	 */
+	function show_modal( item, event ) {
+		event.preventDefault();
+
+		current_media = item;
+
+		pushState( '', {
+			show_modal: true,
+		} );
+	}
 </script>
 
 <Seo title={data.title} />
@@ -27,7 +48,7 @@
 						</figcaption>
 					</figure>
 				{:else if item.mime_type.startsWith( 'image' ) && item.media_details.sizes}
-					<Image media={item} />
+					<Image media={item} on:click={e => show_modal( item, e )} />
 				{:else}
 					<figure>
 						<a download href={item.source_url}>{item.title.rendered}</a>
@@ -39,3 +60,17 @@
 		<p>No media found.</p>
 	{/if}
 </div>
+
+{#if $page.state.show_modal && current_media?.mime_type.startsWith( 'image' )}
+	<Popover on:close={() => history.back()}>
+		<figure>
+			<img
+				alt={current_media.alt_text}
+				height={current_media.media_details.height}
+				src={current_media.source_url}
+				width={current_media.media_details.width}
+			/>
+			<!-- TODO: Caption? -->
+		</figure>
+	</Popover>
+{/if}
