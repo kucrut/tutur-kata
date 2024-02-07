16e4e58aa677a0fea1f1b5bc1b76ad78c40237b2
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-08 01:31:34 +0700

    Popover: Close on clicking outside

--- src/lib/components/popover.svelte
+++ src/lib/components/popover.svelte
@@ -4,9 +4,17 @@
 	/** @type {HTMLDialogElement} */
 	let el;
 
+	/** @param {MouseEvent} event */
+	function handle_click_outside( event ) {
+		if ( event.target === el ) {
+			el.close();
+		}
+	}
+
 	onMount( () => {
 		el.showModal();
 	} );
 </script>
 
+<svelte:body on:click={handle_click_outside} />
 <dialog bind:this={el} class="popover" on:close><slot /></dialog>
