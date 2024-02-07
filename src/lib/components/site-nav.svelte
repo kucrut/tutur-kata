b8e5052c32c3611df4551b903c3164bba86a40c2
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-02-07 18:21:49 +0700

    SiteNav: Remove Blog item

--- src/lib/components/site-nav.svelte
+++ src/lib/components/site-nav.svelte
@@ -1,11 +1,8 @@
 <script>
-	import { get_blog_path } from '$lib/utils/path';
 	import { page } from '$app/stores';
 
 	/** @type {import('$types').NavItem[]} */
 	export let items = [];
-
-	const blog_path = get_blog_path();
 
 	$: aria_current = ( /** @type {string} */ path ) => ( $page.url.pathname === path ? 'page' : null );
 </script>
@@ -15,7 +12,6 @@
 		<a aria-current={aria_current( $page.url.pathname )} href="/">{$page.data.wp_info.name}</a>
 	</span>
 
-	<a aria-current={aria_current( blog_path )} href={blog_path}>Blog</a>
 	{#each items as item}
 		<a aria-current={aria_current( item.path )} href={item.path}>{item.label}</a>
 	{/each}
