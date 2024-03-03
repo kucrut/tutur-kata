d8bd2a38350242f49a07ddb6f5ccb5a25635864c
Dzikri Aziz <dzikri.aziz@gmail.com>
2024-03-03 20:51:10 +0700

    Add env var: PUBLIC_RESUME_URL

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,6 +3,7 @@
 interface ImportMetaEnv {
 	readonly PUBLIC_BLOG_PATH: string;
 	readonly PUBLIC_DATETIME_LOCALE: string;
+	readonly PUBLIC_RESUME_URL: string;
 	readonly WP_API_APP_AUTH_TYPE: string;
 	readonly WP_API_APP_AUTH: string;
 	readonly WP_API_ROOT_URL: string;
